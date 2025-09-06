ef993e19d37df84f46219d22bde51746818f4888
cuzurmyhabit <s2472@e-mirim.hs.kr>
2025-09-06 23:15:30 +0900

    feat: chat 페이지

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Plus, Send, Search, Edit3, Trash2, MessageCircle } from 'lucide-react';
+
+export default Chat;
