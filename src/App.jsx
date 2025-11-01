bca80adebd21b277984f8ddf66cd876e9774f136
yunsiwoong <s2407@e-mirim.hs.kr>
2025-11-01 18:30:12 +0900

    fix:페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { LogIn } from 'lucide-react'
 import Login from '../src/pages/Login'
 import Chat from '../src/pages/Chat'
 import Signup from './pages/Signup'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-
-      {/* <Chat /> */}
-      {/* <Login/> */}
-      <Signup/>
-
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
